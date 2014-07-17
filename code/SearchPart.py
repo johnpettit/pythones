268f7391ec3135fea1334c212cce1b7ac0a3084b
johnpettit <john.pettit@cox.net>
2014-07-17 00:21:40 -0700

    Second    more searching

---
+++ code/SearchPart.py
@@ -0,0 +1,24 @@
+from pyelasticsearch import ElasticSearch
+import json
+
+es = ElasticSearch('http://192.168.2.90:9200/')
+
+
+print "Enter a search term. XXX to quit."
+
+
+data = input()
+while(data != 'XXX'):
+
+    query = {'query': {
+        'prefix': {'FirstName':data}}}
+
+    #res3 = es.search(query, index='contacts')
+    res3 = es.search(query, index='names')
+
+    res3j = json.dumps(res3)
+
+    print(res3j)
+
+    print "Enter a search term. XXX to quit."
+    data = input()
---
+++ code/SearchWildcard.py
@@ -0,0 +1,24 @@
+from pyelasticsearch import ElasticSearch
+import json
+
+es = ElasticSearch('http://192.168.2.90:9200/')
+
+data = raw_input("Enter search term. XXX to exit. --> ")
+
+while(data != 'XXX'):
+
+    #data = data + '?'
+    print(data)
+    #query = {"query": {"wildcard": {"FirstName":data}}}
+    #query = {"size":"1", "query": {"query_string": {"query" : "FirstName:" + data}}}
+    #query = {"version": "true", "query": {"query_string": {"query" : "FirstName:" + data}}}
+    query = {"fields": ["FirstName","LastName"], "query": {"query_string": {"query" : "FirstName:" + data}}}
+    #query = {"query": {"query_string": {"query" : "FirstName:" + data}}}
+    print(query)
+    res3 = es.search(query, index='names')
+
+    res3j = json.dumps(res3,indent=4,sort_keys=True)
+
+    print(res3j)
+
+    data = raw_input("Enter a serch term. XXX to exit. --> ")
