d53ac841d69e979a19b4c1f70d3c05996d1abb49
John Pettit <john.pettit@cox.net>
2014-08-23 15:34:51 -0700

    Part 3

--- code/EVBase.py
+++ code/EVBase.py
@@ -1,9 +1,16 @@
 class EVBase:
     """Base Class for EV Data Objects"""
-    pass
+    internalCount = 0
+
+    def __init__(self):
+        self.internalCount = self.internalCount + 1
+
+    def showCount(self):
+        return self.internalCount
 
 
 # self test
 
 jp = EVBase()
 print(jp.__doc__)
+print(jp.showCount())
