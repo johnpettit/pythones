5a3d5f8d8693ca32dbf4d25907d3b736073b55c4
John Pettit <john.pettit@cox.net>
2014-09-20 18:04:00 -0700

    New Elasticsearch lib changes

--- code/CreateIndex.py
+++ code/CreateIndex.py
@@ -8,27 +8,27 @@ index= {'mappings': {
             'address': {
                 'properties': {
                     'id': {'type': 'long', 'store':'yes'},
-                    'FirstName': { 'type':'string','store':'yes','index':'analyzed'},
-                    'LastName': { 'type':'string','store':'yes','index':'analyzed'},
-                    'NickName': { 'type':'string','store':'yes','index':'analyzed'},
-                    'Street1': { 'type':'string','store':'yes','index':'analyzed'},
-                    'Street2': { 'type':'string','store':'yes','index':'analyzed'},
-                    'City': { 'type':'string','store':'yes','index':'analyzed'},
-                    'State': { 'type':'string','store':'yes','index':'analyzed'},
-                    'Postal': { 'type':'string','store':'yes','index':'analyzed'},
-                    'Country': { 'type':'string','store':'yes','index':'analyzed'},
-                    'CreateDate': { 'type':'date','store':'yes','format':'MM/dd/YYYY'},
-                    'ModifiedDate': { 'type':'date','store':'yes','format':'MM/dd/YYYY'},
+                    'FirstName': {'type':'string','store':'yes','index':'analyzed'},
+                    'LastName': {'type':'string','store':'yes','index':'analyzed'},
+                    'NickName': {'type':'string','store':'yes','index':'analyzed'},
+                    'Street1': {'type':'string','store':'yes','index':'analyzed'},
+                    'Street2': {'type':'string','store':'yes','index':'analyzed'},
+                    'City': {'type':'string','store':'yes','index':'analyzed'},
+                    'State': {'type':'string','store':'yes','index':'analyzed'},
+                    'Postal': {'type':'string','store':'yes','index':'analyzed'},
+                    'Country': {'type': 'string', 'store': 'yes','index':'analyzed'},
+                    'CreateDate': {'type': 'date', 'store': 'yes', 'format': 'MM/dd/YYYY'},
+                    'ModifiedDate': {'type': 'date', 'store': 'yes', 'format': 'MM/dd/YYYY'},
                     }
                }
             }
         }
 
-res = es.delete_index('names')
+res = es.indices.delete(index='names')
 print(res)
-res2 = es.create_index('names',index)
+res2 = es.indices.create(index='names', body=index)
 print(res2)
-res3 = es.get_mapping('names')
+res3 = es.indices.get_mapping(index='names')
 print(res3)
 
 
