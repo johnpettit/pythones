6c77d86ef3224bfa2dcf18f49af04277fcb86df8
John Pettit <john.pettit@cox.net>
2014-09-20 18:28:00 -0700

    Progress

--- code/file.py
+++ code/file.py
@@ -6,27 +6,30 @@ es = Elasticsearch('192.168.2.90:9200/')
 
 acs = []
 count = 0
-with open('data.csv','rb') as datafile:
-    datareader = csv.reader(datafile,delimiter=',')
-    for row in datareader:
-        print(row)
-        count += 1
-        new = Account()
-        new.fields['FirstName'] = row[0]
-        new.fields['LastName'] = row[1]
-        new.fields['NickName'] = row[2]
-        new.FirstName = row[0]
-        new.LastName = row[1]
-        new.NickName = row[2]
-        new.Street1 = row[3]
-        new.Street2 = row[4]
-        new.City = row[5]
-        new.State = row[6]
-        new.Postal = row[7]
-        new.Country = row[8]
-        new.CreateDate = row[9]
-        new.ModifiedDate = row[10]
-        acs.append(new)
+with open('data.csv', 'r') as datafile:
+    datareader = csv.reader(datafile, delimiter=',')
+    if datareader:
+        for row in datareader:
+            print(row)
+            count += 1
+            new = Account()
+            new.fields['FirstName'] = row[0]
+            new.fields['LastName'] = row[1]
+            new.fields['NickName'] = row[2]
+            new.FirstName = row[0]
+            new.LastName = row[1]
+            new.NickName = row[2]
+            new.Street1 = row[3]
+            new.Street2 = row[4]
+            new.City = row[5]
+            new.State = row[6]
+            new.Postal = row[7]
+            new.Country = row[8]
+            new.CreateDate = row[9]
+            new.ModifiedDate = row[10]
+            acs.append(new)
+    else:
+        print("NOGO")
 
 for acc in acs:
     #for k,v in acc.fields.items():
@@ -35,7 +38,8 @@ for acc in acs:
     #print "Last Name  : " + acc.fields['LastName']
     print("First Name  : " + acc.FirstName)
     print("Last Name  : " + acc.LastName)
-    es.index('names','address', {'FirstName':acc.FirstName,'LastName':acc.LastName,'NickName':acc.NickName,'Street1':acc.Street1,'Street2':acc.Street2,'City':acc.City,'State':acc.State,'Postal':acc.Postal,'Country':acc.Country,'CreateDate':acc.CreateDate,'ModifiedDate':acc.ModifiedDate})
+    print("Create Date :  " + acc.CreateDate)
+    es.index(index='names', doc_type='address', body={'FirstName': acc.FirstName, 'LastName': acc.LastName, 'NickName': acc.NickName, 'Street1': acc.Street1, 'Street2': acc.Street2, 'City': acc.City, 'State':acc.State,'Postal':acc.Postal,'Country':acc.Country,'CreateDate':acc.CreateDate,'ModifiedDate':acc.ModifiedDate})
 
 es.indices.refresh(index='names')
 
