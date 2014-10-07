0db9a804b46f5cafb78f451f0c1b078446799219
John Pettit <john.pettit@cox.net>
2014-10-07 05:54:50 -0700

    New file

---
+++ code/ClassWork.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+
+class JPClass:
+    """This is my class"""
+    _name = ''
+
+    def __init__(self):
+        self._name = 'Fred'
+
+    def changeName(self):
+        self._name = 'Mike'
+
+    def
+
+# self test
+
+mine = JPClass()
+print(mine._name)
+mine.changeName()
+print(mine._name)
